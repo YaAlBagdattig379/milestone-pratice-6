f6dc874ebe52fc301f7c1a47bc6496d193a7a10b
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-06 20:15:51 +0530

    added

--- place-holder.js
+++ place-holder.js
@@ -1,14 +1,24 @@
-// const boudi = {  money: 200 }
-// const boudi2 = { money: 200 }
-// if(JSON.stringify(boudi) ===JSON.stringify(boudi2)){
-// to get info 
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(res => res.json())
-.then(data => console.log(data))
-// function for input button 
+
+// function for product buttn 
 function productBtn(){
-    const inputProduct = document.getElementById("input-product");
-    const productValue = inputProduct.value;
-    inputProduct.value = '';
-    console.log(productValue);
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => allData(data))
+
+
+}
+const showResult = document.getElementById("show-result");
+function allData(productData){
+    for(const product of productData){
+        console.log(product)
+//     //     const emailId = product.name;
+        const div = document.createElement('div');
+        div.classList.add('ms-5')
+        div.innerHTML = `
+            Name: ${product.username} idNum:${   product.id}
+            Email : ${product.email}
+        `  
+        showResult.appendChild(div);   
+    }
+     
 }
